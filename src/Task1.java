7c7b4c31c22f1178788cefb9991dcebd292cfb04
ElenaNikolaeva2022 <107610168+ElenaNikolaeva2022@users.noreply.github.com>
2022-12-13 12:17:14 +0300

    Update Task1.java
    Добавлен пример.

--- src/Task1.java
+++ src/Task1.java
@@ -7,25 +7,41 @@
 
 import java.util.Scanner;
 
+// public class Task1 {
+//     static Scanner iScanner = new Scanner(System.in, "ibm866");
+//     static String input;
+
+//     public static void main(String[] args) {
+//         System.out.println(inpFloat());
+//     }
+
+//     public static float inpFloat() {
+
+//         System.out.println("Введите число c точкой: ");
+//         input = iScanner.next();
+
+//         while (!input.matches("[+-]?([0-9]*[.])?[0-9]+")) {
+//             System.out.println("Неверный ввод. Введите число c точкой: ");
+//             input = iScanner.next();
+//         }
+//         iScanner.close();
+//         return Float.parseFloat(input);
+//     }
+// }
+
 public class Task1 {
-    static Scanner iScanner = new Scanner(System.in, "ibm866");
-    static String input;
-
+    // запрашивать у пользователя число типа float, пока не введет верный формат
     public static void main(String[] args) {
-        System.out.println(inpFloat());
-    }
-
-    public static float inpFloat() {
-
-        System.out.println("Введите число c точкой: ");
-        input = iScanner.next();
-
-        while (!input.matches("[+-]?([0-9]*[.])?[0-9]+")) {
-            System.out.println("Неверный ввод. Введите число c точкой: ");
-            input = iScanner.next();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String floatPattern = "([+-]?(\\d+\\.)?\\d+)";
+        while (!input.matches(floatPattern)) {
+            System.out.println("Введен неверный формат " + input);
+            input = scanner.nextLine();
         }
-        iScanner.close();
-        return Float.parseFloat(input);
+
+        float num = Float.parseFloat(input);
+        System.out.println(num + 1);
     }
 }
 
